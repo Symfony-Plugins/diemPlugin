69762ea3b1624ce9c95c3c458c2d052459938b5c
ornicar2 <ornicar2@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-02-08 17:39:14 -0600

    [Diem] - added comments on base dmUser actions class
    git-svn-id: http://svn.symfony-project.com/plugins/diemPlugin/trunk@27766 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- dmCorePlugin/plugins/dmUserPlugin/modules/dmUser/lib/BasedmUserActions.class.php
+++ dmCorePlugin/plugins/dmUserPlugin/modules/dmUser/lib/BasedmUserActions.class.php
@@ -3,6 +3,44 @@
 class BasedmUserActions extends myFrontModuleActions
 {
 
+  /**
+   * Handle dmUser/signin form validation and authenticates the user
+   */
+  public function executeSigninWidget(dmWebRequest $request)
+  {
+    $form = $this->forms['DmSigninFront'];
+
+    if ($request->isMethod('post') && $request->hasParameter($form->getName()))
+    {
+      if ($form->bindAndValid($request))
+      {
+        $this->getUser()->signin($form->getValue('user'), $form->getValue('remember'));
+
+        return $this->redirect($request->getReferer());
+      }
+    }
+  }
+
+  /**
+   * Handle dmUser/form form validation and creates the user account, then authenticates the user
+   */
+  public function executeFormWidget(dmWebRequest $request)
+  {
+    $form = $this->forms['DmUser'];
+
+    if ($request->isMethod('post') && $request->hasParameter($form->getName()))
+    {
+      if ($form->bindAndValid($request))
+      {
+        $user = $form->save();
+
+        $this->getUser()->signin($user);
+
+        return $this->redirect($request->getReferer());
+      }
+    }
+  }
+
   public function executeSignin(dmWebRequest $request)
   {
     $request->setParameter('dm_page', dmDb::table('DmPage')->fetchSignin());
@@ -28,37 +66,5 @@ class BasedmUserActions extends myFrontModuleActions
     $signoutUrl = sfConfig::get('dm_security_success_signout_url', $request->getReferer());
 
     $this->redirect('' != $signoutUrl ? $signoutUrl : '@homepage');
-  }
-  
-  public function executeSigninWidget(dmWebRequest $request)
-  {
-    $form = $this->forms['DmSigninFront'];
-
-    if ($request->isMethod('post') && $request->hasParameter($form->getName()))
-    {
-      if ($form->bindAndValid($request))
-      {
-        $this->getUser()->signin($form->getValue('user'), $form->getValue('remember'));
-
-        return $this->redirect($request->getReferer());
-      }
-    }
-  }
-
-  public function executeFormWidget(dmWebRequest $request)
-  {
-    $form = $this->forms['DmUser'];
-
-    if ($request->isMethod('post') && $request->hasParameter($form->getName()))
-    {
-      if ($form->bindAndValid($request))
-      {
-        $user = $form->save();
-        
-        $this->getUser()->signin($user);
-
-        return $this->redirect($request->getReferer());
-      }
-    }
   }
 }
